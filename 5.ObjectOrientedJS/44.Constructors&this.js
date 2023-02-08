ce36212ef81c2a7fe19123392a9c86e780f60aef
Jona <jona@jonas-mbp-2.lan>
2023-02-07 22:47:20 -0500

    start of 6

--- 5.ObjectOrientedJS/44.Constructors&this.js
+++ 5.ObjectOrientedJS/44.Constructors&this.js
@@ -1,3 +1,5 @@
+// Constructor - is a special type of function that is used to create objects and set their initial properties. The constructor function is invoked with the "new" operator, which creates a new instance of the object and sets the "this" keyword to reference the new object. The constructor can then define properties and methods for the object.
+
 // Person constructor
 function Person(name, dob) {
   this.name = name;
---
+++ 6.OOP.BookList/app.js
@@ -0,0 +1,31 @@
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+
+// UI Constructor
+function UI() {}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+
+    // Get form values
+    const title = document.getElementById('title').value,
+         author = document.getElementById('author').value,
+         isbn = document.getElementById('isbn').value;
+         console.log(title, author, isbn);
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Add book to list
+
+
+    e.preventDefault();
+});
